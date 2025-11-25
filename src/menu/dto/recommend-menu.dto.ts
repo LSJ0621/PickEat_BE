017f7371137b7bcd058b249a223399c82b8893b3
Seongjae_Lim <seongjae_lim@Seongjae-Limui-MacBookPro.local>
2025-11-25 21:27:44 +0900

    UI/UX를 고려한 로직 수정 및 디자인 수정

--- src/menu/dto/recommend-menu.dto.ts
+++ src/menu/dto/recommend-menu.dto.ts
@@ -1,7 +1,31 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+class RecommendMenuLocationDto {
+  @IsNumber()
+  lat: number;
+
+  @IsNumber()
+  lng: number;
+}
 
 export class RecommendMenuDto {
   @IsString()
   @IsNotEmpty()
   prompt: string;
+
+  @IsOptional()
+  @IsString()
+  requestAddress?: string;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => RecommendMenuLocationDto)
+  requestLocation?: RecommendMenuLocationDto;
 }
