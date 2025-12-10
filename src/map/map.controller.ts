74d1bc97d5ececcc5f39487368d291fc1b533f76
Seongjae_Lim <seongjae_lim@Seongjae-Limui-MacBookPro.local>
2025-12-10 19:34:34 +0900

    추천 이력 pagination 적용

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,28 +1,15 @@
-import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guard/jwt.guard';
 import { SearchRestaurantsDto } from '../search/dto/search-restaurants.dto';
 import { MapService } from './map.service';
 
 @Controller('map')
+@UseGuards(JwtAuthGuard)
 export class MapController {
   constructor(private readonly mapService: MapService) {}
 
   @Post('restaurants')
-  @UseGuards(JwtAuthGuard)
   async getRestaurantMarkers(@Body() dto: SearchRestaurantsDto) {
     return this.mapService.getRestaurantMarkers(dto);
-  }
-
-  @Get('google-places/photo')
-  async getGooglePlacePhoto(
-    @Query('photoName') photoName: string,
-    @Query('maxHeightPx') maxHeightPx?: number,
-    @Query('maxWidthPx') maxWidthPx?: number,
-  ) {
-    return this.mapService.getGooglePlacePhoto(
-      photoName,
-      maxHeightPx,
-      maxWidthPx,
-    );
   }
 }
--- src/map/map.module.ts
+++ src/map/map.module.ts
@@ -1,13 +1,12 @@
 import { HttpModule } from '@nestjs/axios';
 import { Module, forwardRef } from '@nestjs/common';
-import { GoogleModule } from '../external/google/google.module';
 import { NaverModule } from '../external/naver/naver.module';
 import { SearchModule } from '../search/search.module';
 import { MapController } from './map.controller';
 import { MapService } from './map.service';
 
 @Module({
-  imports: [HttpModule, forwardRef(() => SearchModule), GoogleModule, NaverModule],
+  imports: [HttpModule, forwardRef(() => SearchModule), NaverModule],
   controllers: [MapController],
   providers: [MapService],
   exports: [MapService],
